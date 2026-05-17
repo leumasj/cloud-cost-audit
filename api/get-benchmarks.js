d665b05c8b9f11a953e1f808f414f649a23181c7
leumas <leumas@example.com>
2026-05-17 17:55:38 +0200

    superbasefix

--- api/get-benchmarks.js
+++ api/get-benchmarks.js
@@ -12,7 +12,7 @@ const sentry = require('./lib/_sentry');
 
 const supabase = createClient(
   process.env.SUPABASE_URL,
-  process.env.SUPABASE_ANON_KEY
+  process.env.SUPABASE_SERVICE_ROLE_KEY
 );
 
 module.exports = async function handler(req, res) {
