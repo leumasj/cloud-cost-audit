b96c995d97af9ee53a1d0aeb5b9f46256ae504c2
leumas <leumas@example.com>
2026-05-24 11:32:30 +0200

    new

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,24 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
       output: {
-        manualChunks: {
-          vendor: ['react', 'react-dom'],
-        },
-      },
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (id.includes('react') || id.includes('react-dom')) {
+              return 'react-vendor';
+            }
+            if (id.includes('stripe')) {
+              return 'stripe-vendor';
+            }
+            return 'vendor';
+          }
+        }
+      }
     },
-  },
+    chunkSizeWarningLimit: 600,
+  }
 })
