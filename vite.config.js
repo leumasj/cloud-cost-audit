0c21ab623ba8adf055a853a0b7cf06cfb78cbc80
leumas <leumas@example.com>
2026-05-25 23:24:15 +0200

    new

--- vite.config.js
+++ vite.config.js
@@ -8,17 +8,13 @@ export default defineConfig({
       output: {
         manualChunks(id) {
           if (id.includes('node_modules')) {
-            if (id.includes('react') || id.includes('react-dom')) {
-              return 'react-vendor';
-            }
-            if (id.includes('stripe')) {
-              return 'stripe-vendor';
-            }
+            if (id.includes('react-dom')) return 'react-dom';
+            if (id.includes('react')) return 'react';
             return 'vendor';
           }
         }
       }
     },
-    chunkSizeWarningLimit: 600,
+    chunkSizeWarningLimit: 500,
   }
 })
